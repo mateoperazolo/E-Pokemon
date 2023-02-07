45bebfe11d2decd715c816523205118eece85337
Mateo Perazolo <mateoperazolo@gmail.com>
2023-02-07 20:21:52 +0100

    header+section1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-
+import React from "react";
+import './header.css'; 
+import './section1.css';
+import pokemonImagen from './images/pokemon.png';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>E-Pokemon</h1>
-        <a>Home</a>
-        
-        <a>Contacto</a>
-      </nav>
+      <header className="Header">
+          <h1 className="TitleE-Pokemon">e-Pokemon</h1>
+        <nav className="navHeader">
+          <div >
+            <a className="LicksHeader">Home</a>
+            <a className="LicksHeader" href="/src/Pokemons/index.js">Pokemons</a>
+            <a className="LicksHeader" href="/src/Support/index.js">Support</a>
+            <a className="LicksHeader" href="/src/Contact Us/index.js">Contact Us</a>
+          </div>
+        </nav>
+        <input className="searchHeader" type={Text}></input>
+
+      </header>
+
+      <section className="section1">
+        <div className="contentSection1">
+          <h2 className="h2Section1">If you wera a Pokemon, what would it be?</h2>
+          <h1 className="h1Section1">Fine your Pokemon
+          <br/>
+           soul mate!</h1>
+          <p1 className="p1Section1">
+            Search, read, learn, compare and selects the pokemon that
+            <br/>
+             best represents you.
+          </p1>
+        </div>
+        <img className="logoPokemon" src={pokemonImagen}/>   
+      </section>  
     </div>
   );
 }
