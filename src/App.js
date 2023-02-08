afcdca17b1af3e023cf361fe008500b7b63ad9ca
Mateo Perazolo <mateoperazolo@gmail.com>
2023-02-08 20:06:41 +0100

    section2 and section3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import './header.css'; 
 import './section1.css';
+import './section2.css';
+import './section3.css';
 import pokemonImagen from './images/pokemon.png';
+import pokeballImagen from './images/icons8-pokeball-48.png';
+import Pokemon1 from './images/pokemon1.png';
+import Pokemon2 from './images/pokemon2.png';
+
 
 function App() {
   return (
@@ -11,9 +17,15 @@ function App() {
         <nav className="navHeader">
           <div >
             <a className="LicksHeader">Home</a>
-            <a className="LicksHeader" href="/src/Pokemons/index.js">Pokemons</a>
-            <a className="LicksHeader" href="/src/Support/index.js">Support</a>
-            <a className="LicksHeader" href="/src/Contact Us/index.js">Contact Us</a>
+            <a 
+            className="LicksHeader" 
+            href="/src/Pokemons/index.js">Pokemons</a>
+            <a 
+            className="LicksHeader"
+            href="/src/Support/index.js">Support</a>
+            <a 
+            className="LicksHeader" 
+            href="/src/Contact Us/index.js">Contact Us</a>
           </div>
         </nav>
         <input className="searchHeader" type={Text}></input>
@@ -34,6 +46,75 @@ function App() {
         </div>
         <img className="logoPokemon" src={pokemonImagen}/>   
       </section>  
+
+      <section className="section2">
+        <h1 className="h1Section2">The important ones!</h1>
+
+        <div className="contentSection2">
+          <article className="articleSection2">
+            <h2>Pokemon name</h2>
+            <div className="contentArticleSection2">
+              <img className="pictureSection2" src={pokeballImagen}/>
+              <h3>Pokemon description</h3>
+            </div>
+            <p1>ability,ability,ability,ability,ability...</p1>
+          </article>
+          
+          <article className="articleSection2">
+            <h2>Pokemon name</h2>
+            <div className="contentArticleSection2">
+              <img className="pictureSection2" src={pokeballImagen}/>
+              <h3>Pokemon description</h3>
+            </div>
+            <p1>ability,ability,ability,ability,ability...</p1>
+          </article>
+        </div>
+
+        <div className="contentSection2">  
+          <article className="articleSection2">
+            <h2>Pokemon name</h2>
+            <div className="contentArticleSection2">
+              <img className="pictureSection2" src={pokeballImagen}/>
+              <h3>Pokemon description</h3>
+            </div>
+            <p1>ability,ability,ability,ability,ability...</p1>
+          </article>
+          
+          <article className="articleSection2">
+            <h2>Pokemon name</h2>
+            <div className="contentArticleSection2">
+              <img className="pictureSection2" src={pokeballImagen}/>
+              <h3>Pokemon description</h3>
+            </div>
+            <p1>ability,ability,ability,ability,ability...</p1>
+          </article>
+
+        </div>
+
+      </section>
+
+      <section className="section3">
+        <h1 className="h1Section3">The Crew!</h1>
+        <div className="contentSection3">
+          <div className="contentPerson">
+            <img className="imagenCrew" src={Pokemon2}/>
+            <h2>Mateo Perazolo</h2>
+            <p>La visita a la capital francesa tiene lugar después de que el presidente ucraniano 
+                  viajara a Londres, donde se reunió con el primer ministro, Rishi Sunak, el rey, 
+                  Carlos III, y dio un discurso ante el Parlamento
+            </p>
+          </div>
+
+          <div>
+            <img className="imagenCrew" src={Pokemon1}/>
+            <h2>Jose Manuel Rosa</h2>
+            <p>El mandatario reafirmó su compromiso para trabajar con el gigante asiático, 
+              pero remarcó que la defensa es la prioridad luego del derribo del globo espía 
+              que sobrevolaba el territorio estadounidense
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
